4b62b0b5ecd68f4e1616cfb07cbdbce5812dd8aa
Megh Bhalerao <megh.bhalerao@gmail.com>
2020-03-31 20:20:31 +0530

    some basic function structure

--- iisc_codes/q1/knn.py
+++ iisc_codes/q1/knn.py
@@ -1,1 +1,19 @@
 import numpy as np
+
+def get_k():
+    return something
+
+def L2_distance():
+    
+    return something
+
+def cosine_distance():
+    return something
+
+def main():
+
+
+    return 0
+
+if __name__ == "__main__":
+    main()
