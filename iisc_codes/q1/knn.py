52708d3c3b5beebcbdbdea5389e5cbae12c01475
Megh Bhalerao <megh.bhalerao@gmail.com>
2020-03-31 20:31:38 +0530

    some function adds

--- iisc_codes/q1/knn.py
+++ iisc_codes/q1/knn.py
@@ -1,14 +1,22 @@
 import numpy as np
 
-def get_k():
-    return something
+# =============================================================================
+# def get_k():
+#     return something
+# 
+# def L2_distance():
+#     
+#     return something
+# 
+# def cosine_distance():
+#     return something
+# =============================================================================
 
-def L2_distance():
-    
-    return something
-
-def cosine_distance():
-    return something
+def unpickle(file):
+    import pickle
+    with open(file, 'rb') as fo:
+        dict = pickle.load(fo, encoding='bytes')
+    return dict
 
 def main():
 
