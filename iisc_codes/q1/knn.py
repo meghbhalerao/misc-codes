06b3f8cc21693705dcfc754bcda8a00212c2a931
Megh Bhalerao <megh.bhalerao@gmail.com>
2020-04-01 11:24:02 +0530

    added normalization

--- iisc_codes/q1/knn.py
+++ iisc_codes/q1/knn.py
@@ -3,8 +3,12 @@ import pickle
 import time 
 
 something = 0
-def get_k():
-    return something
+# =============================================================================
+# def get_k(data_val,data_train,K):
+#     for k in range(1,K):
+#         
+#     return something
+# =============================================================================
 
 def L2_distance():
     
@@ -12,6 +16,13 @@ def L2_distance():
 
 def cosine_distance():
     return something
+
+def normalize(matrix):
+    mean = np.mean(matrix.flatten())
+    sigma = np.std(matrix.flatten())
+    matrix = (matrix - mean)/sigma
+    return matrix 
+    
 
 start = time.time()
 data_train_full = np.loadtxt("/Users/megh/Work/misc/data/cifar-10-batches-py/txt_data/data_train.txt")
@@ -24,10 +35,12 @@ print("Loading data took: ", (end-start)/60, " minutes")
 labels_train_full = np.expand_dims(labels_train_full,axis=1)
 labels_test = np.expand_dims(labels_test,axis=1)
 # Concatenation of the labels with the data
+for row in range(data_train_full.shape[0]):
+    data_train_full[row,:] = normalize(data_train_full[row,:])    
 data_train_full = np.concatenate((labels_train_full,data_train_full),axis=1)
 data_test = np.concatenate((labels_test,data_test),axis=1)
 # Before this a random shuffling of the training data can be done once I figure out why the np shuffle funcrtion is not working
-
 # Splitting the training data further into training and validation data
 data_train = data_train_full[0:45000,:]
 data_val = data_train_full[45000:50000,:]
+# Preprocessing the training, validation and test data
