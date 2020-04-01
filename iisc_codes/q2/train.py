bdf88cf7344bc896ce28058bcb5796f6d073b589
Megh Bhalerao <megh.bhalerao@gmail.com>
2020-04-01 20:34:14 +0530

    training and validation added

--- iisc_codes/q2/train.py
+++ iisc_codes/q2/train.py
@@ -9,6 +9,7 @@ from torchvision import models
 from data import CIFAR10_train
 from data_val import CIFAR10_val
 from torch.utils.data import Dataset, DataLoader
+from torch.optim.lr_scheduler import ReduceLROnPlateau
 
 def normalize(matrix):
     mean = np.mean(matrix.flatten())
@@ -54,27 +55,10 @@ print("Training Data Samples: ", len(train_loader.dataset))
 
 
 optimizer = optim.Adam(model.parameters(), lr = learning_rate, betas = (0.9,0.999), weight_decay = 0.00005)
-scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer, [clr])
-
+scheduler = ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=10, verbose=False, threshold=0.0001, threshold_mode='rel', cooldown=0, min_lr=0, eps=1e-08)
 ############### CHOOSING THE LOSS FUNCTION ###################
-if which_loss == 'dc':
-    loss_fn  = MCD_loss
-if which_loss == 'dcce':
-    loss_fn  = DCCE
-if which_loss == 'ce':
-    loss_fn = CE
-if which_loss == 'mse':
-    loss_fn = MCD_MSE_loss
-############## STORING THE HISTORY OF THE LOSSES #################
-avg_val_loss = 0
-total_val_loss = 0
-best_val_loss = 2000
-best_tr_loss = 2000
-total_loss = 0
-total_dice = 0
-best_idx = 0
-best_n_val_list = []
-val_avg_loss_list = []
+loss_fn = x
+
 ################ TRAINING THE MODEL##############
 for ep in range(num_epochs):
     start = time.time()
@@ -89,13 +73,12 @@ for ep in range(num_epochs):
         image = subject['image']
         mask = subject['gt']
         # Loading images into the GPU and ignoring the affine
-        image, mask = image.float().to(device), mask.float().to(device)
+        image, mask = image.float(), mask.float()
         #Variable class is deprecated - parameteters to be given are the tensor, whether it requires grad and the function that created it   
         image, mask = Variable(image, requires_grad = True), Variable(mask, requires_grad = True)
         # Making sure that the optimizer has been reset
         optimizer.zero_grad()
-        # Forward Propagation to get the output from the models
-        torch.cuda.empty_cache()
+
         output = model(image.float())
         # Computing the loss
         loss = loss_fn(output.double(), mask.double(),n_classes)
@@ -104,27 +87,9 @@ for ep in range(num_epochs):
         #Updating the weight values
         optimizer.step()
         #Pushing the dice to the cpu and only taking its value
-        curr_loss = dice_loss(output[:,0,:,:,:].double(), mask[:,0,:,:,:].double()).cpu().data.item()
-        #train_loss_list.append(loss.cpu().data.item())
-        total_loss+=curr_loss
-        # Computing the average loss
-        average_loss = total_loss/(batch_idx + 1)
-        #Computing the dice score 
-        curr_dice = 1 - curr_loss
-        #Computing the total dice
-        total_dice+= curr_dice
-        #Computing the average dice
-        average_dice = total_dice/(batch_idx + 1)
+        
         scheduler.step()
-        torch.cuda.empty_cache()
-    print("Epoch Training dice:" , average_dice)      
-    if average_dice > 1-best_tr_loss:
-        best_tr_idx = ep
-        best_tr_loss = 1 - average_dice
-    total_dice = 0
-    total_loss = 0     
-    print("Best Training Dice:", 1-best_tr_loss)
-    print("Best Training Epoch:", best_tr_idx)
+
     # Now we enter the evaluation/validation part of the epoch    
     model.eval        
     for batch_idx, (subject) in enumerate(val_loader):
@@ -133,35 +98,7 @@ for ep in range(num_epochs):
             mask = subject['gt']
             image, mask = image.to(device), mask.to(device)
             output = model(image.float())
-            curr_loss = dice_loss(output[:,0,:,:,:].double(), mask[:,0,:,:,:].double()).cpu().data.item()
-            total_loss+=curr_loss
-            # Computing the average loss
-            average_loss = total_loss/(batch_idx + 1)
-            #Computing the dice score 
-            curr_dice = 1 - curr_loss
-            #Computing the total dice
-            total_dice+= curr_dice
-            #Computing the average dice
-            average_dice = total_dice/(batch_idx + 1)
-
-    print("Epoch Validation Dice: ", average_dice)
-    torch.save(model, model_path + which_model  + str(ep) + ".pt")
-    if ep > save_best:
-        keep_list = np.argsort(np.array(val_avg_loss_list))
-        keep_list = keep_list[0:save_best]
-        for j in range(ep):
-            if j not in keep_list:
-                if os.path.isfile(os.path.join(model_path + which_model  + str(j) + ".pt")):
-                    os.remove(os.path.join(model_path + which_model  + str(j) + ".pt"))
-        
-        print("Best ",save_best," validation epochs:", keep_list)
-
-    total_dice = 0
-    total_loss = 0
-    stop = time.time()   
-    val_avg_loss_list.append(1-average_dice)  
-    print("Time for epoch:",(stop - start)/60,"mins")    
-    sys.stdout.flush()
+            
 
 
 
