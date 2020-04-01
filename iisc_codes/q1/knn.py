f5871402824e563c1084f713b91e689576e59471
Megh Bhalerao <megh.bhalerao@gmail.com>
2020-04-01 11:01:31 +0530

    set up the validation data

--- iisc_codes/q1/knn.py
+++ iisc_codes/q1/knn.py
@@ -14,15 +14,20 @@ def cosine_distance():
     return something
 
 start = time.time()
-data_train = np.loadtxt("/Users/megh/Work/misc/data/cifar-10-batches-py/txt_data/data_train.txt")
-labels_train = np.loadtxt("/Users/megh/Work/misc/data/cifar-10-batches-py/txt_data/labels_train.txt")
+data_train_full = np.loadtxt("/Users/megh/Work/misc/data/cifar-10-batches-py/txt_data/data_train.txt")
+labels_train_full = np.loadtxt("/Users/megh/Work/misc/data/cifar-10-batches-py/txt_data/labels_train.txt")
 data_test = np.loadtxt("/Users/megh/Work/misc/data/cifar-10-batches-py/txt_data/data_test.txt")
 labels_test = np.loadtxt("/Users/megh/Work/misc/data/cifar-10-batches-py/txt_data/labels_test.txt")
 end = time.time()
 print("Loading data took: ", (end-start)/60, " minutes")
 # Expanding dimentions of labels for concatenation with image data
-labels_train = np.expand_dims(labels_train,axis=1)
+labels_train_full = np.expand_dims(labels_train_full,axis=1)
 labels_test = np.expand_dims(labels_test,axis=1)
 # Concatenation of the labels with the data
-data_train = np.concatenate((labels_train,data_train),axis=1)
+data_train_full = np.concatenate((labels_train_full,data_train_full),axis=1)
 data_test = np.concatenate((labels_test,data_test),axis=1)
+# Before this a random shuffling of the training data can be done once I figure out why the np shuffle funcrtion is not working
+
+# Splitting the training data further into training and validation data
+data_train = data_train_full[0:45000,:]
+data_val = data_train_full[45000:50000,:]
