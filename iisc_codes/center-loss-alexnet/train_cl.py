b19e4fa21afdb12ddce4d7e0cfeb410ffaa590df
Megh Bhalerao <megh.bhalerao@gmail.com>
2020-04-20 14:40:58 +0530

    improved formatting

--- iisc_codes/center-loss-alexnet/train_cl.py
+++ iisc_codes/center-loss-alexnet/train_cl.py
@@ -14,26 +14,33 @@ import pandas as pd
 import datetime
 from center_loss import CenterLoss
 import copy
+
 def MSE_loss(mat1,mat2):
     diff = mat1.flatten() - mat2.flatten()
     loss = sum(diff*diff)
     return loss
+
 num_classes = 10
 df_train_full = pd.read_csv("train.csv")
+
 # Defining the train and validation sets clearly
 data_train = df_train_full[0:45000]
 data_val = df_train_full[45000:50000]
+
 # Setting up the dataloader
 dataset_train = CIFAR10_train(data_train)
 train_loader = DataLoader(dataset_train,batch_size= 1,shuffle=True,num_workers=4)
 dataset_valid = CIFAR10_val(data_val)
 val_loader = DataLoader(dataset_valid, batch_size=1,shuffle=True,num_workers = 4)
+
 # Defining a pre-existing model in torch
 model = models.alexnet(pretrained=True)
 model.classifier[6] = nn.Linear(4096,num_classes)
+
 # Freezing the convolutional layer weights
 for param in model.features.parameters():
     param.requires_grad = False
+    
 # Freezing the classifier layer weights except the last layer
 for n in range(6):
     for param in model.classifier[n].parameters():
@@ -49,6 +56,7 @@ optimizer_centloss = torch.optim.SGD(center_loss.parameters(), lr=0.5)
 
 optimizer = optim.Adam(model.classifier[6].parameters(), lr = 0.1, betas = (0.9,0.999), weight_decay = 0.00005)
 scheduler = ReduceLROnPlateau(optimizer, mode='max', factor=0.1, patience=10, verbose=False, threshold=0.003, threshold_mode='rel', cooldown=0, min_lr=0, eps=1e-08)
+
 ###### PARAMETERS NEEDED TO BE MONITORED ##########
 train_acc = 0
 val_acc = 0
@@ -56,6 +64,7 @@ train_loss = 0
 val_loss = 0
 num_epochs = 100
 alpha = 0.1
+
 ################ TRAINING THE MODEL ##############
 for ep in range(num_epochs):
     start = time.time()
@@ -65,16 +74,17 @@ for ep in range(num_epochs):
     print("Learning rate:", optimizer.param_groups[0]['lr'])
     model.train
     for batch_idx, (subject) in enumerate(train_loader):
+        
         # Load the subject and its ground truth
         image = subject['image']
         mask = subject['gt']
+        
         # Loading images into the GPU and ignoring the affine
         image, mask = image.float(), mask.float()
-        #Variable class is deprecated - parameteters to be given are the tensor, whether it requires grad and the function that created it   
-        # Making sure that the optimizer has been reset
         optimizer.zero_grad()
         output = model(image.float())
         feature = feature_model(image.float())
+        
         # Computing the loss
         mask = mask.T
         # Combination of the center loss and standard MSE loss
@@ -88,8 +98,10 @@ for ep in range(num_epochs):
         # Updating the parameters of the center loss as well as the network
         optimizer_centloss.step()
         optimizer_centloss.zero_grad()
+        
         #Updating the weight values
         optimizer.step()
+        
         #Pushing the ground truth and predicted class to the cpu
         mask = mask.cpu().detach().numpy()
         output = output.cpu().detach().numpy()
@@ -97,6 +109,7 @@ for ep in range(num_epochs):
         temp[0,np.argmax(output)] =  1
         train_acc+=sum(mask*temp)
         train_loss+=loss.cpu().detach().numpy()
+        
     print("Training Accuracy for epoch # ", ep, " is: ",train_acc/(batch_idx+1))
     print("Training Loss for epoch # ", ep, " is: ",train_loss/(batch_idx+1))
 
